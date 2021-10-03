83e039079248d077d0eead58fe0c6237146b9cfe
Electrum9.1 <vikram.bhagavatula@gmail.com>
2021-10-02 21:13:21 -0700

    Removed to-binary.py from gitignore, also overwrote new-alu-tests.tv

--- to-binary.py
+++ to-binary.py
@@ -1,6 +1,15 @@
 #!/bin/python3
-ignore = {'_', '\n'}
-with open("./alu-tests.tv") as f:
-    for l in f:
-        s = bin(int(''.join(c for c in filter(lambda x: x not in ignore, l)), 16))[2:]
-        print(s)
+
+#ignore = {'_', '\n'}
+def hexToBin(hs):
+    return bin(int(hs, 16))[2:]
+
+def main():
+    with open("./alu-tests.tv") as f:
+        for l in f:
+            #s = bin(int(''.join(c for c in filter(lambda x: x not in ignore, l)), 16))[2:]
+            s = '_'.join(map(hexToBin, l.split('_')))
+            print(s)
+
+if __name__=="__main__":
+    main()
