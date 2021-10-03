4accfdbd136bf96129adfc48e5292f76c44d93f8
Electrum9.1 <vikram.bhagavatula@gmail.com>
2021-10-02 17:27:13 -0700

    Initial version of testbench made (still haven't verified it compiles)

--- testbench.v
+++ testbench.v
@@ -1,7 +1,19 @@
 `timescale 1ns/100ps
+
 module testbench();
 
-reg [4:0] test_cases [20:0]; // loaded with all test cases from test vector file
+/* 
+test_cases is loaded with all test cases from test vector file.
+There are 21 tests total, where the inputs and outputs for each tests are encoded
+in a 100 length bit vector. 
+
+The structure of the bit vector is as follows (from MSB to LSB):
+- The leftmost 3 bits = f
+- The next couple 32 bit blocks are a, b, y (in that order)
+- Final bit is Zero
+
+*/
+reg [99:0] test_cases [20:0]; 
 
 // These hold inputs to be fed into the ALU 
 reg [2:0] f; // Control input
@@ -13,25 +25,16 @@ wire y, zero;
 alu dut(.f(f), .a(a), .b(b), .y(y), .zero(zero));
 
 initial begin
-  #3;
-  a = 0;
-  b = 0;
-  #1 $display("a=%d, b=%d, s=%d, cout=%d", a, b, s, cout);
+  $readmemb("alu-tests.tv", test_cases)
+  integer i;
 
-  #3;
-  a = 1;
-  b = 0;
-  #1 $display("a=%d, b=%d, s=%d, cout=%d", a, b, s, cout);
-
-  #3;
-  a = 0;
-  b = 1;
-  #1 $display("a=%d, b=%d, s=%d, cout=%d", a, b, s, cout);
-
-  #3;
-  a = 1;
-  b = 1;
-  #1 $display("a=%d, b=%d, s=%d, cout=%d", a, b, s, cout);
+  for (i = 0; i < 21; i + 1) begin
+    #3
+    f = test_cases[0][99:97]  
+    a = test_cases[0][96:65]  
+    b = test_cases[0][65:34]  
+    #1 $display("a=%d, b=%d, s=%d, cout=%d", a, b, y, zero);
+  end
 end
 
 endmodule
